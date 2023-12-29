89185dfc9887bc0047dc6a42e11a1a79fd141b04
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-12-29 10:25:48 -0300

    STYLES(MainActivity.kt)

--- AppCouseJetBizCard/app/src/main/java/com/demate/appcousejetbizcard/MainActivity.kt
+++ AppCouseJetBizCard/app/src/main/java/com/demate/appcousejetbizcard/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.demate.appcousejetbizcard.ui.theme.AppCouseJetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,9 +44,20 @@ class MainActivity : ComponentActivity() {
 fun CreateBizCard(name: String, company: String, phone: String, email: String) {
     Surface(
         modifier = Modifier.fillMaxSize().fillMaxHeight(),
-        color = MaterialTheme.colorScheme.background
     ) {
-        Text(text = "Hello $name!")
+        Card (modifier = Modifier
+            .width(300.dp)
+            .height(200.dp)
+            .padding(16.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = CardDefaults.cardElevation(8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+        ) {
+
+
+        }
     }
 }
 
