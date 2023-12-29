039bb53178d2ec0353a25b2d76728be7d549a0de
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-12-29 10:40:28 -0300

    STYLES(MainActivity.kt)

--- AppCouseJetBizCard/app/src/main/java/com/demate/appcousejetbizcard/MainActivity.kt
+++ AppCouseJetBizCard/app/src/main/java/com/demate/appcousejetbizcard/MainActivity.kt
@@ -3,22 +3,26 @@ package com.demate.appcousejetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.demate.appcousejetbizcard.ui.theme.AppCouseJetBizCardTheme
@@ -43,20 +47,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBizCard(name: String, company: String, phone: String, email: String) {
     Surface(
-        modifier = Modifier.fillMaxSize().fillMaxHeight(),
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight(),
     ) {
-        Card (modifier = Modifier
-            .width(300.dp)
-            .height(200.dp)
-            .padding(16.dp),
+        Card(
+            modifier = Modifier
+                .width(300.dp)
+                .height(200.dp)
+                .padding(16.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = CardDefaults.cardElevation(8.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.White,
             ),
         ) {
-
-
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.Black),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f))
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "profile_img",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
