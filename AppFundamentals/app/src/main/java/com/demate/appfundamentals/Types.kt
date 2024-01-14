01616c88a476b2308e645956fcb16df86cb70152
joaodematejr <joao.demate@alquimidiacorp.com.br>
2024-01-14 06:22:30 -0300

    FINISH(types.kt)

--- AppFundamentals/app/src/main/java/com/demate/appfundamentals/Types.kt
+++ AppFundamentals/app/src/main/java/com/demate/appfundamentals/Types.kt
@@ -13,6 +13,18 @@ fun main() {
     val myInt: Int = 20
     val myByte: Byte = 127
     val myShort: Short = 32767
-    
 
+
+    /**
+     * var - mutable ... int and long
+     */
+    val number: Int = 10
+    var number2: Long = 2L
+
+    /**
+     * var - mutable ... double and float
+     */
+
+    val pi: Double = 3.14
+    val float: Float = 3.14F
 }
