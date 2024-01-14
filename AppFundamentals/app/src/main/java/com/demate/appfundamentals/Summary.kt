5f91700e09b92b58a9dba661544dbf52870a24c9
joaodematejr <joao.demate@alquimidiacorp.com.br>
2024-01-14 06:51:01 -0300

    FINISH(Summary.kt)

---
+++ AppFundamentals/app/src/main/java/com/demate/appfundamentals/Summary.kt
@@ -0,0 +1,38 @@
+package com.demate.appfundamentals
+
+fun main() {
+
+    /**
+     * Variables
+     * var: mutable
+     * val: immutable
+     * Best practice: use val as much as possible
+     *
+     * val name: String = "John" - explicitly initialize a variable
+     *
+     * Another way to initialize a variables
+     * val name = "John"
+     * val age = 30
+     * Basic Types in Kotlin
+     * (Whole Numbers)
+     * Byte: 8 bits
+     * Short: 16 bits
+     * Int: 32 bits
+     * Long: 64 bits
+     * (Decimal Numbers)
+     * Float: 32 bits
+     * Double: 64 bits
+     * (Other Types)
+     * Boolean: true or false
+     * Char: character
+     * String: sequence of characters
+     * Number
+     * Any
+     * Unit
+     * Nothing
+     * Array
+     * Collection
+     */
+
+
+}
