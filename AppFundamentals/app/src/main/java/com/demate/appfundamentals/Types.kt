915b7299b9e3c6da7b26f1600ccffef45b58c026
joaodematejr <joao.demate@alquimidiacorp.com.br>
2024-01-14 06:01:45 -0300

    INIT(types.kt)

---
+++ AppFundamentals/app/src/main/java/com/demate/appfundamentals/Types.kt
@@ -0,0 +1,18 @@
+package com.demate.appfundamentals
+
+fun main() {
+
+    /**
+     * Variables
+     * var: mutable
+     * val: immutable
+     * Best practice: use val as much as possible
+     */
+
+    val myString: String = "João"
+    val myInt: Int = 20
+    val myByte: Byte = 127
+    val myShort: Short = 32767
+    
+
+}
