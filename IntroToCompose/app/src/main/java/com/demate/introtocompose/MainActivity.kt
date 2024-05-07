49b6acaa9a458545549965721d2d1e9c4ff8fdb2
João Dematé Jr <joaodematejr@gmail.com>
2024-05-07 08:24:04 -0300

    STYLES(button tap)

--- IntroToCompose/app/src/main/java/com/demate/introtocompose/MainActivity.kt
+++ IntroToCompose/app/src/main/java/com/demate/introtocompose/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.demate.introtocompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,8 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.demate.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,27 +49,46 @@ fun MyApp() {
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFFE0E0E0)
     ) {
-        CreateCircle()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "100,00", style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            CreateCircle()
+        }
     }
 }
 
 @Preview
 @Composable
 fun CreateCircle() {
+    var moneyCounter = 0
     Card(
         modifier = Modifier
             .padding(3.dp)
             .size(105.dp)
             .clickable {
-                Log.d("TAG", "Tapped")
+                moneyCounter = moneyCounter + 1
             },
         shape = CircleShape,
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Box(contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(150.dp)
+                .height(200.dp),
+            contentAlignment = Alignment.Center
+        ) {
             Text(
                 text = "TAP",
-                modifier = Modifier,
+                textAlign = TextAlign.Center
             )
         }
     }
