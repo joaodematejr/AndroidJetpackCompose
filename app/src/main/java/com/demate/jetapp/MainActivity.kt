bc01203b96863755b248581f90385dc81cc98ac1
João Dematé Jr <joaodematejr@gmail.com>
2024-06-10 19:17:47 -0300

    FINISH(app)

--- app/src/main/java/com/demate/jetapp/MainActivity.kt
+++ app/src/main/java/com/demate/jetapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.demate.jetapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,9 +25,14 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -39,6 +45,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.demate.jetapp.components.InputField
 import com.demate.jetapp.ui.theme.JetAppTheme
+import com.demate.jetapp.utils.calculateTotalPerPerson
+import com.demate.jetapp.utils.calculateTotalTip
 import com.demate.jetapp.widgets.RoundIconButton
 import java.text.NumberFormat
 import java.util.Locale
@@ -74,6 +82,7 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(20.dp)
             .height(150.dp)
             .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
         color = Color(0x0FC40EA3)
@@ -101,9 +110,26 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 @Preview
 @Composable
 fun MainContext() {
-    BillForm() { billAmt ->
-        {
+    val splitByState = remember {
+        mutableIntStateOf(0)
+    }
 
+    val range = IntRange(start = 1, endInclusive = 100)
+
+    val tipAmountState = remember {
+        mutableDoubleStateOf(0.0)
+    }
+
+    val totalPerPersonState = remember {
+        mutableDoubleStateOf(0.0)
+    }
+
+    Column(modifier = Modifier.padding(all = 12.dp)) {
+        BillForm(
+            splitByState = splitByState,
+            tipAmountState = tipAmountState,
+            totalPerPersonState = totalPerPersonState
+        ) {
         }
     }
 }
@@ -111,6 +137,10 @@ fun MainContext() {
 @Composable
 fun BillForm(
     modifier: Modifier = Modifier,
+    range: IntRange = 1..100,
+    splitByState: MutableState<Int>,
+    tipAmountState: MutableState<Double>,
+    totalPerPersonState: MutableState<Double>,
     onValChange: (String) -> Unit = {}
 ) {
     val totalBillState = remember {
@@ -120,6 +150,15 @@ fun BillForm(
         totalBillState.value.trim().isNotEmpty()
     }
     val keyboardController = LocalSoftwareKeyboardController.current
+
+    val sliderPositionState = remember {
+        mutableFloatStateOf(0f)
+    }
+
+    val tipPercentage = (sliderPositionState.floatValue * 100).toInt()
+
+    TopHeader(totalPerPerson = totalPerPersonState.value)
+
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -142,42 +181,99 @@ fun BillForm(
                     onValChange(totalBillState.value.trim())
                     keyboardController?.hide()
                 })
-            //if (validState) {
-            Row(
-                modifier = Modifier.padding(3.dp),
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Split", modifier = Modifier.align(
-                        alignment = Alignment.CenterVertically
-                    )
-                )
-                Spacer(modifier = Modifier.width(120.dp))
+            if (validState) {
                 Row(
-                    modifier = Modifier.padding(horizontal = 3.dp),
-                    horizontalArrangement = Arrangement.End
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
                 ) {
-                    RoundIconButton(imageVector = Icons.Default.Remove, onClick = { /*TODO*/ })
                     Text(
-                        text = "2",
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 9.dp, end = 9.dp)
+                        text = "Split", modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        )
                     )
-                    RoundIconButton(imageVector = Icons.Default.Add, onClick = { /*TODO*/ })
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(imageVector = Icons.Default.Remove, onClick = {
+                            splitByState.value =
+                                if (splitByState.value > 1) splitByState.value - 1 else 1
+                            totalPerPersonState.value = calculateTotalPerPerson(
+                                totalBill = totalBillState.value.toDouble(),
+                                splitBy = splitByState.value,
+                                tipPercentage = tipPercentage
+                            )
+                        })
+                        Text(
+                            text = "${splitByState.value}",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp)
+                        )
+                        RoundIconButton(imageVector = Icons.Default.Add, onClick = {
+                            splitByState.value =
+                                if (splitByState.value < range.last) splitByState.value + 1 else 1
+                            totalPerPersonState.value = calculateTotalPerPerson(
+                                totalBill = totalBillState.value.toDouble(),
+                                splitBy = splitByState.value,
+                                tipPercentage = tipPercentage
+                            )
+                        })
+                    }
+                }
+                //Tip Row
+                Row(
+                    modifier = Modifier.padding(
+                        horizontal = 3.dp, vertical = 12.dp
+                    )
+                ) {
+                    Text(
+                        text = "Tip",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(200.dp))
+                    Text(text = "${tipAmountState.value}")
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "$tipPercentage %")
+                    Spacer(modifier = Modifier.height(14.dp))
+                    //Slider
+                    Slider(
+                        value = sliderPositionState.floatValue,
+                        onValueChange = { newVal ->
+                            sliderPositionState.floatValue = newVal
+                            tipAmountState.value =
+                                calculateTotalTip(totalBillState.value.toDouble(), tipPercentage)
+                            totalPerPersonState.value = calculateTotalPerPerson(
+                                totalBill = totalBillState.value.toDouble(),
+                                splitBy = splitByState.value,
+                                tipPercentage = tipPercentage
+                            )
+                        },
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        steps = 5,
+                        onValueChangeFinished = {
+                            Log.d("207", "207")
+                        }
+                    )
                 }
             }
-            //}
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetAppTheme {
         MyApp {
-            Text(text = "ola Mundo")
+            MainContext()
         }
     }
 }
