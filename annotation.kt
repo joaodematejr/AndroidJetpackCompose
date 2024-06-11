8f3c7bb594755c279f060ee287ee7cf58197d275
João Dematé Jr <joaodematejr@gmail.com>
2024-06-11 19:12:01 -0300

    Update and rename annotation to annotation.kt

---
+++ annotation.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val engine = Engine()
+    val turbo = TurboEngine()
+    val car = Car(engine, turbo)
+    
+    car.startCar()
+    car.engine.start()
+    car.turbo.start()
+}
+
+class Engine() {
+    fun start() {
+        println("Engine X started...")
+    }
+ 
+}
+class TurboEngine() {
+    fun start() {
+        println("Start Turbo engine X")
+    }
+}
+class Car(val engine: Engine, val turbo: TurboEngine) {
+    //val engine = Engine() // not good
+    fun startCar() {
+        println("starting car... ${engine.start()}")
+    }
+}
