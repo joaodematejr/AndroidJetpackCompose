60adf6aada2e696b60e502333777462351caecd9
João Dematé Jr <joaodematejr@gmail.com>
2024-06-13 11:33:53 -0300

    finish navigation

--- MovieApp/app/src/main/java/com/demate/movieapp/screens/details/DetailsScreen.kt
+++ MovieApp/app/src/main/java/com/demate/movieapp/screens/details/DetailsScreen.kt
@@ -1,32 +1,58 @@
 package com.demate.movieapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(navController: NavController, string: String?) {
     // Details Screen
-    Surface(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("Details Screen$string", style = MaterialTheme.typography.bodyLarge)
-        }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                ),
+                title = {
+                    Text("Movie")
+                },
+                navigationIcon = {
+                    Button(onClick = { navController.popBackStack() }) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ) { it ->
+        it.calculateTopPadding()
+        Text("Details Screen$string", style = MaterialTheme.typography.bodyLarge)
     }
+
+    //Surface(
+    //    modifier = Modifier
+    //        .fillMaxHeight()
+    //        .fillMaxWidth()
+    //) {
+    //    Column(
+    //        horizontalAlignment = Alignment.CenterHorizontally,
+    //        verticalArrangement = Arrangement.Center
+    //    ) {
+    //        Text("Details Screen$string", style = MaterialTheme.typography.bodyLarge)
+    //        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
+    //        Button(onClick = { navController.popBackStack() }) {
+    //            Text("Go Back")
+    //        }
+    //    }
+//
+    //}
 
 }
