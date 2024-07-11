af050621bb233cac0e8b226e591afb89bdafda0e
João Dematé Jr <joaodematejr@gmail.com>
2024-07-10 21:24:36 -0300

    FEAT(Create DAO)

--- JetWeatherForecast/app/src/main/java/com/demate/jetweatherforecast/data/WeatherDao.kt
+++ JetWeatherForecast/app/src/main/java/com/demate/jetweatherforecast/data/WeatherDao.kt
@@ -1,7 +1,11 @@
 package com.demate.jetweatherforecast.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.demate.jetweatherforecast.model.Favorite
 import kotlinx.coroutines.flow.Flow
 
@@ -12,5 +16,25 @@ interface WeatherDao {
 
     @Query("SELECT * FROM fav_tbl WHERE city = :city")
     suspend fun getFavById(city: String): Favorite
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: Favorite) {
+        insertFavorite(favorite)
+    }
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateFavorite(favorite: Favorite) {
+        updateFavorite(favorite)
+    }
+
+    @Query("DELETE FROM fav_tbl")
+    suspend fun deleteAllFavorite(favorite: Favorite) {
+        deleteAllFavorite(favorite)
+    }
+
+    @Delete
+    suspend fun deleteFavorite(favorite: Favorite) {
+        deleteFavorite(favorite)
+    }
 
 }
