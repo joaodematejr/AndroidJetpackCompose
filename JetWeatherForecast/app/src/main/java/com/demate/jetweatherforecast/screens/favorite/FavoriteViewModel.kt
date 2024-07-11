ca10b5e89e772ff7e6d26f0afc58c51df63ae2d5
João Dematé Jr <joaodematejr@gmail.com>
2024-07-10 22:38:52 -0300

    FEAT(Create ViewModel)

---
+++ JetWeatherForecast/app/src/main/java/com/demate/jetweatherforecast/screens/favorite/FavoriteViewModel.kt
@@ -0,0 +1,55 @@
+package com.demate.jetweatherforecast.screens.favorite
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.demate.jetweatherforecast.model.Favorite
+import com.demate.jetweatherforecast.resository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(private val repository: WeatherRepository) :
+    ViewModel() {
+    private val _favList = MutableStateFlow<List<Favorite>>(emptyList())
+    val favList = _favList.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getFavoriteWeather().distinctUntilChanged()
+                .collect { listOfFav ->
+                    if (listOfFav.isNotEmpty()) {
+
+                    } else {
+                        _favList.value = listOfFav
+                    }
+                }
+        }
+    }
+
+    fun insertFavorite(favorite: Favorite) = viewModelScope.launch {
+        repository.insertFavorite(favorite)
+    }
+
+    fun updateFavorite(favorite: Favorite) = viewModelScope.launch {
+        repository.updateFavorite(favorite)
+    }
+
+    fun deleteFavorite(favorite: Favorite) = viewModelScope.launch {
+        repository.deleteFavorite(favorite)
+    }
+
+    fun deleteAllFavorite() = viewModelScope.launch {
+        repository.deleteAllFavorite()
+    }
+
+    fun getFavoriteByCity(city: String) = viewModelScope.launch {
+        repository.getFavoriteByCity(city)
+    }
+
+
+}
