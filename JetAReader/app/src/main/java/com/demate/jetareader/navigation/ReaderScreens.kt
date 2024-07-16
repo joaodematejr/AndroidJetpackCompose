fe49e7e138c6404321a38eb2ee698a58b1d4145b
João Dematé Jr <joaodematejr@gmail.com>
2024-07-16 15:34:54 -0300

    FEAT(create Screens enuns)

--- JetAReader/app/src/main/java/com/demate/jetareader/navigation/ReaderScreens.kt
+++ JetAReader/app/src/main/java/com/demate/jetareader/navigation/ReaderScreens.kt
@@ -1,4 +1,27 @@
 package com.demate.jetareader.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            ReaderHomeScreen.name -> ReaderHomeScreen
+            SearchScreen.name -> SearchScreen
+            DetailScreen.name -> DetailScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            null -> ReaderStatsScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized.")
+        }
+    }
 }
